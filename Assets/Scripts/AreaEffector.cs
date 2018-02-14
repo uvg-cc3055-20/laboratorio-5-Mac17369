a1228edd5114e43bc8fc2bc719dd7ecddc79d243
Mac17369 <Mac17369@uvg.edu.gt>
2018-02-14 09:18:40 -0600

    Actualizacion
    Cambios varios

---
+++ Assets/Scripts/AreaEffector.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AreaEffector : MonoBehaviour {
+    public float time = 0;
+    public bool activated;
+    public GameObject child;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        time += Time.deltaTime;
+        if (time >= 3) {
+            activated = !activated;
+            child.SetActive(activated);
+            time = 0;
+        }
+    }
+}
